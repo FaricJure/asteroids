d31bf2f4521adde4b1b029bce167fb4b0c9a4193
FaricJure <jure.faric@gmail.com>
2025-11-01 22:46:57 +0100

    Working game

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def main():
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
